b44e7c615f2c5a4e0a7d306012c8fca36714c2cc
juissi999 <jussi.kaatiala@gmail.com>
2021-04-04 11:18:59 +0300

    Change DOM creation to script-level

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ const refreshGraph = () => {
 
     // render graph
     renderGraph(
-      document.getElementById('app'),
+      document.getElementById('graph'),
       newNodes,
       newEdges,
       document.getElementById('hierarchychkbx').checked
@@ -70,17 +70,18 @@ const refreshGraph = () => {
     // something went wrong, create empty graph
     document.getElementById('statustxt').textContent = `${error}`
 
-    renderGraph(document.getElementById('app'), [], [], false)
+    renderGraph(document.getElementById('graph'), [], [], false)
   }
 }
 
 window.addEventListener('load', () => {
   // define callback-functions for DOM-elements
+  createLayout('app')
   document.getElementById('createbtn').addEventListener('click', refreshGraph)
   document.getElementById('clearbtn').addEventListener('click', () => {
     document.getElementById('jsonstr').value = ''
     document.getElementById('statustxt').textContent = ''
-    renderGraph(document.getElementById('app'), [], [], false)
+    renderGraph(document.getElementById('graph'), [], [], false)
   })
   document
     .getElementById('leavesselect')
@@ -90,7 +91,7 @@ window.addEventListener('load', () => {
     .addEventListener('change', refreshGraph)
   document.getElementById('helpbtn').addEventListener('click', () => {
     renderGraph(
-      document.getElementById('app'),
+      document.getElementById('graph'),
       helpNodes(STYLES),
       helpEdges(STYLES),
       false
@@ -101,3 +102,66 @@ window.addEventListener('load', () => {
   resetTextBox()
   refreshGraph()
 })
+
+const createLayout = (elId) => {
+  const app = document.getElementById(elId)
+  const container = document.createElement('div')
+
+  container.setAttribute('class', 'container')
+
+  const title = document.createElement('h2')
+  title.innerText = 'jsonviz'
+  container.appendChild(title)
+
+  const block1 = document.createElement('div')
+  block1.setAttribute('class', 'block')
+  block1.innerHTML =
+    'Display JSON structure graphically. <span id="statustxt" class="error"></span>'
+  container.appendChild(block1)
+
+  const block2 = document.createElement('div')
+  block2.setAttribute('class', 'block')
+  block2.innerHTML = '<textarea id="jsonstr" rows="6"></textarea>'
+  container.appendChild(block2)
+
+  const block3 = document.createElement('div')
+  block3.setAttribute('class', 'block')
+  block3.innerHTML = `
+    Leaf mode 
+    <select name="leaves" id="leavesselect">
+      <option value="hide">hide</option>
+      <option value="show">show</option>
+      <option value="value">show value</option>
+    </select>`
+  container.appendChild(block3)
+
+  const block4 = document.createElement('div')
+  block4.setAttribute('class', 'block')
+  block4.innerHTML = `
+    <div class="block">
+      <input type="checkbox" id="hierarchychkbx"> <label for="hierarchychkbx">Hierarchical</label>
+    </div>`
+  container.appendChild(block4)
+
+  const block5 = document.createElement('div')
+  block5.setAttribute('class', 'block')
+  block5.innerHTML = `
+    <div class="block">
+      <button id="createbtn">Create graph</button>
+      <button id="clearbtn">Clear textbox</button>
+      <button id="helpbtn">Help</button>
+    </div>`
+  container.appendChild(block5)
+
+  const graph = document.createElement('div')
+  graph.setAttribute('class', 'block')
+  graph.setAttribute('id', 'graph')
+  container.appendChild(graph)
+
+  const block6 = document.createElement('div')
+  block6.setAttribute('class', 'block')
+  block6.innerHTML = `<a href="https://github.com/juissi999/jsonviz">juissi999@github</a>`
+  container.appendChild(block6)
+
+  app.appendChild(container)
+}
