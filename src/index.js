e3314d384deae58a035b34fd243c9ad0ae530e5a
juissi999 <jussi.kaatiala@gmail.com>
2021-04-08 23:46:51 +0300

    Update some styles and dependencies

--- src/index.js
+++ src/index.js
@@ -120,16 +120,18 @@ const createLayout = (elId) => {
   const block5 = document.createElement('div')
   block5.setAttribute('class', 'my-2')
 
-  block5.appendChild(createButton('Create graph', refreshGraph))
   block5.appendChild(
-    createButton('Clear textbox', () => {
+    createButton('Create graph', 'lightblue text-white', refreshGraph)
+  )
+  block5.appendChild(
+    createButton('Clear textbox', 'yellow', () => {
       document.getElementById('jsonstr').value = ''
       document.getElementById('statustxt').textContent = ''
       renderGraph(document.getElementById('graph'), [], [], false)
     })
   )
   block5.appendChild(
-    createButton('Help', () => {
+    createButton('Help', 'green', () => {
       renderGraph(
         document.getElementById('graph'),
         helpNodes(STYLES),
@@ -153,10 +155,10 @@ const createLayout = (elId) => {
   app.appendChild(container)
 }
 
-const createButton = (txt, callback) => {
+const createButton = (txt, classes, callback) => {
   const btn = document.createElement('button')
   btn.appendChild(document.createTextNode(txt))
   btn.addEventListener('click', callback)
-  btn.setAttribute('class', 'mr-1')
+  btn.setAttribute('class', 'mr-1 mw-2 pointer ' + classes)
   return btn
 }
